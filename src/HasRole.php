85a471a78e37c0342d5d69016f6de51f8145af43
Sergey Populov <sergey@saritasa.com>
2018-01-17 20:00:11 +0500

    Add PHP_CodeStiffer, autofixes to code

--- src/HasRole.php
+++ src/HasRole.php
@@ -25,7 +25,7 @@ trait HasRole
     {
         if (is_int($role)) {
             return $this->role_id == $role;
-        } else if (is_string($role)) {
+        } elseif (is_string($role)) {
             return strcasecmp($this->role->slug, $role) == 0;
         } else {
             throw new NotImplementedException("function hasRole() accepts either int (role_id) or string (role name). "
--- src/IHasRole.php
+++ src/IHasRole.php
@@ -9,21 +9,21 @@ interface IHasRole
 {
     /**
      * Relation between Users and Roles tables.
-     * 
+     *
      * @return BelongsTo
-     */    
+     */
     public function role(): BelongsTo;
 
     /**
-     * Get user's role entity 
-     * 
+     * Get user's role entity
+     *
      * @return Role
      */
     public function getRole(): Role;
 
     /**
      * Check, if user has specified role
-     * 
+     *
      * @param int|string $role
      * @return bool
      */
