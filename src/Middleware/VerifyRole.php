cb30f62d0e473ce6f3773e85d126a32d7ca46b0f
DenisGostroushko <g.denis@saritasa.com>
2018-10-05 14:01:10 +0700

    update the middleware for checking role and dependencies of the library

--- src/Middleware/VerifyRole.php
+++ src/Middleware/VerifyRole.php
@@ -3,10 +3,10 @@
 
 namespace Saritasa\Roles\Middleware;
 
-use App\Models\User;
 use Closure;
 use Illuminate\Contracts\Auth\Guard;
 use Illuminate\Http\Request;
+use Saritasa\Roles\IHasRole;
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 
 class VerifyRole
@@ -34,20 +34,24 @@ class VerifyRole
      * @param  \Illuminate\Http\Request $request HTTP Request
      * @param  \Closure $next Next middleware handler
      * @param array ...$roles If user doesn't have any of these roles, access will be denied
-     * @throws AccessDeniedHttpException
+     *
      * @return mixed
+     *
+     * @throws AccessDeniedHttpException
      */
     public function handle(Request $request, Closure $next, ...$roles)
     {
         if (!$this->auth->check()) {
             throw new AccessDeniedHttpException(trans('roles::errors.not_logged_in'));
         }
-        /* @var  User $user */
+
         $user = $this->auth->user();
 
-        foreach ($roles as $role) {
-            if ($user->hasRole($role)) {
-                return $next($request);
+        if ($user instanceof IHasRole) {
+            foreach ($roles as $role) {
+                if ($user->hasRole($role)) {
+                    return $next($request);
+                }
             }
         }
 
